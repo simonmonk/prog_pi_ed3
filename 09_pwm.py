4d9d5e8d24388e029f19519810876c27f4909c5b
simonmonk <evilgeniusauthor@gmail.com>
2020-08-19 09:35:41 +0100

    copied code from 2nd ed

---
+++ 09_pwm.py
@@ -0,0 +1,19 @@
+#09_pwm.py
+
+import RPi.GPIO as GPIO
+
+led_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+
+pwm_led = GPIO.PWM(led_pin, 500)
+pwm_led.start(100)
+
+try:
+    while True:
+      duty_s = input("Enter Brightness (0 to 100):")
+      duty = int(duty_s)
+      pwm_led.ChangeDutyCycle(duty)
+finally:  
+    print("Cleaning up")
+    GPIO.cleanup()
