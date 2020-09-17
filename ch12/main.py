df0c870547ab29524a81e57efe223c80c01d3328
simonmonk <evilgeniusauthor@gmail.com>
2020-09-17 11:45:04 +0100

    added ch12

---
+++ ch12/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+from PCA9685 import PCA9685
+import time
+
+Dir = [
+    'forward',
+    'backward',
+]
+pwm = PCA9685(0x40, debug=False)
+pwm.setPWMFreq(50)
+
+class MotorDriver():
+    def __init__(self):
+        self.PWMA = 0
+        self.AIN1 = 1
+        self.AIN2 = 2
+        self.PWMB = 5
+        self.BIN1 = 3
+        self.BIN2 = 4
+
+    def MotorRun(self, motor, index, speed):
+        if speed > 100:
+            return
+        if(motor == 0):
+            pwm.setDutycycle(self.PWMA, speed)
+            if(index == Dir[0]):
+                print ("1")
+                pwm.setLevel(self.AIN1, 0)
+                pwm.setLevel(self.AIN2, 1)
+            else:
+                print ("2")
+                pwm.setLevel(self.AIN1, 1)
+                pwm.setLevel(self.AIN2, 0)
+        else:
+            pwm.setDutycycle(self.PWMB, speed)
+            if(index == Dir[0]):
+                print ("3")
+                pwm.setLevel(self.BIN1, 0)
+                pwm.setLevel(self.BIN2, 1)
+            else:
+                print ("4")
+                pwm.setLevel(self.BIN1, 1)
+                pwm.setLevel(self.BIN2, 0)
+
+    def MotorStop(self, motor):
+        if (motor == 0):
+            pwm.setDutycycle(self.PWMA, 0)
+        else:
+            pwm.setDutycycle(self.PWMB, 0)
+
+print("this is a motor driver test code")
+Motor = MotorDriver()
+
+print("forward 2 s")
+Motor.MotorRun(0, 'forward', 100)
+Motor.MotorRun(1, 'forward', 100)
+time.sleep(2)
+
+print("backward 2 s")
+Motor.MotorRun(0, 'backward', 100)
+Motor.MotorRun(1, 'backward', 100)
+time.sleep(2)
+
+print("stop")
+Motor.MotorStop(0)
+Motor.MotorStop(1)
