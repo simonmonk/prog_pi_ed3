3f8f52b0126c898e8ee0f5bfaddb9ea13f7a5db8
simonmonk <evilgeniusauthor@gmail.com>
2020-09-18 09:56:23 +0100

    added ch12

--- ch12/motor_driver_i2c.py
+++ ch12/motor_driver_i2c.py
@@ -16,14 +16,14 @@ class MotorDriver():
         self.BIN2 = 4
 
     def set_motors(self, left_pwm, left_dir, right_pwm, right_dir):
-        pwm.setDutycycle(self.PWMA, left_pwm)
+        pwm.setDutycycle(self.PWMA, left_pwm * 100)
         if left_dir == 1:
             pwm.setLevel(self.AIN1, 0)
             pwm.setLevel(self.AIN2, 1)
         else:
             pwm.setLevel(self.AIN1, 1)
             pwm.setLevel(self.AIN2, 0)
-        pwm.setDutycycle(self.PWMB, right_pwm)
+        pwm.setDutycycle(self.PWMB, right_pwm * 100)
         if right_dir == 1:
             pwm.setLevel(self.BIN1, 0)
             pwm.setLevel(self.BIN2, 1)
@@ -31,8 +31,42 @@ class MotorDriver():
             pwm.setLevel(self.BIN1, 1)
             pwm.setLevel(self.BIN2, 0)
 
+    def forward(self, seconds=0, speed=1.0):
+        self.set_motors(speed, 0, speed, 0)
+        if seconds > 0:
+            time.sleep(seconds)
+            self.stop()
+
+    def stop(self):
+        self.set_motors(0, 0, 0, 0)
+
+    def reverse(self, seconds=0, speed=1.0):
+        self.set_motors(speed, 1, speed, 1)
+        if seconds > 0:
+            time.sleep(seconds)
+            self.stop()
+
+    def left(self, seconds=0, speed=0.5):
+        self.set_motors(speed, 0, speed, 1)
+        if seconds > 0:
+            time.sleep(seconds)
+            self.stop()
+
+    def right(self, seconds=0, speed=0.5):
+        self.set_motors(speed, 1, speed, 0)
+        if seconds > 0:
+            time.sleep(seconds)
+            self.stop()
 
 m = MotorDriver()
 m.set_motors(50, 1, 25, 0)
 input("stop")
-m.set_motors(0, 0, 0, 0)
+m.stop()
+input("forward")
+m.forward(3, 0.5)
+input("stop")
+m.stop()
+input("left")
+m.left(5)
+input("stop")
+m.stop()
