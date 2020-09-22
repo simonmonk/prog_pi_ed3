e1867a8979365bbe66f608662be4e2a7b45b7a94
simonmonk <evilgeniusauthor@gmail.com>
2020-09-22 11:41:18 +0100

    renamed

--- ch12/12_01_rover_web.py
+++ ch12/12_01_rover_web.py
@@ -1,26 +1,24 @@
-# 12_01_rover_avoiding.py
-from gpiozero import DistanceSensor
+# 12_02_rover_web.py
+
+from bottle import route, run, template, request
 from motor_driver_i2c import MotorDriver
-import time, random
 
 motors = MotorDriver()
-rangefinder = DistanceSensor(echo=18, trigger=17)
 
-def turn_randomly():
-    turn_time = random.randint(1, 3)
-    if random.randint(1, 2) == 1:
-        motors.left(turn_time) 
-    else:
-        motors.right(turn_time)
-    motors.stop()
-
-while True:
-    distance = rangefinder.distance * 100 # convert to cm
-    print(distance)
-    if distance < 20:
-        motors.stop()
-    elif distance < 50:
-        turn_randomly()
-    else:
+# Handler for the home page
+@route('/')
+def index():
+    cmd = request.GET.get('command', '')
+    if cmd == 'f':
         motors.forward()
-    time.sleep(0.2)
+    elif cmd == 'l':
+        motors.left(0, 0.5) # turn at half speed
+    elif cmd == 's':
+        motors.stop()
+    elif cmd == 'r':
+        motors.right(0, 0.5)
+    elif cmd == 'b':
+        motors.reverse(0, 0.3) # reverse slowly
+    return template('home.tpl')
+        
+run(host="0.0.0.0", port=80)
--- ch12/12_02_rover_avoiding.py
+++ ch12/12_02_rover_avoiding.py
@@ -1,24 +1,26 @@
-# 12_02_rover_web.py
-
-from bottle import route, run, template, request
+# 12_01_rover_avoiding.py
+from gpiozero import DistanceSensor
 from motor_driver_i2c import MotorDriver
+import time, random
 
 motors = MotorDriver()
+rangefinder = DistanceSensor(echo=18, trigger=17)
 
-# Handler for the home page
-@route('/')
-def index():
-    cmd = request.GET.get('command', '')
-    if cmd == 'f':
-        motors.forward()
-    elif cmd == 'l':
-        motors.left(0, 0.5) # turn at half speed
-    elif cmd == 's':
+def turn_randomly():
+    turn_time = random.randint(1, 3)
+    if random.randint(1, 2) == 1:
+        motors.left(turn_time) 
+    else:
+        motors.right(turn_time)
+    motors.stop()
+
+while True:
+    distance = rangefinder.distance * 100 # convert to cm
+    print(distance)
+    if distance < 20:
         motors.stop()
-    elif cmd == 'r':
-        motors.right(0, 0.5)
-    elif cmd == 'b':
-        motors.reverse(0, 0.3) # reverse slowly
-    return template('home.tpl')
-        
-run(host="0.0.0.0", port=80)
+    elif distance < 50:
+        turn_randomly()
+    else:
+        motors.forward()
+    time.sleep(0.2)
